3abb5647317ea3cdebbff73c36f135cb945531a0
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-08-31 12:54:33 -0400

    function max two numbers

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -5,6 +5,9 @@
   // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
   // ---------------------
 
+  function max(num1, num2) {
+    if num1 > num2
+  }
 
 
   // ---------------------
